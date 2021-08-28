a39a7b31334486aeef548d0ea3144064767d5572
vineetuppal <89680615+vineetuppal@users.noreply.github.com>
2021-08-28 13:42:56 +0530

    Update Activity_05.py

--- Activity_05.py
+++ Activity_05.py
@@ -1,8 +1,7 @@
 input_line = input("Enter 5 numbers:-\n")
 numbers = input_line.split(' ')
 list=[numbers]
-print(list)
 sum = 0
 for i in numbers:
 	sum = sum + int(i)
-print(sum)
+print("Sum of all numbers is",sum)
